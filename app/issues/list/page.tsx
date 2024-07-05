b3bd4a4baf92ced909bbc226e579516539dfcd92
guoliang <g.zhang724@mybvc.ca>
2024-07-04 18:32:30 -0600

    feat: paginate issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -7,11 +7,16 @@ import { IssueStatusBadge } from "../../components";
 import IssueActions from "./IssueActions";
 import { Status } from "@prisma/client";
 import { Issue } from "@prisma/client";
+import Pagination from "@/app/components/Pagination";
 
 const IssuesPage = async ({
   searchParams,
 }: {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    page: string;
+  };
 }) => {
   const columns: {
     label: string;
@@ -27,17 +32,25 @@ const IssuesPage = async ({
     ? { [searchParams.orderBy]: "asc" }
     : undefined;
 
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
     where: {
       status: searchParams.status,
     },
     orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize,
+  });
+
+  const issueCount = await prisma.issue.count({
+    where: { status: searchParams.status },
   });
 
   return (
     <div>
       <IssueActions />
-
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -72,6 +85,11 @@ const IssuesPage = async ({
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemCount={100}
+      ></Pagination>
     </div>
   );
 };
